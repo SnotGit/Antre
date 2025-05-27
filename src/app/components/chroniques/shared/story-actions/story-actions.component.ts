a758cccb7120ba90f7432988800f9c04eb620bbb
SnotGit <crocpoms@aol.com>
2025-05-27 19:46:46 +0200

    update

---
+++ src/app/components/chroniques/shared/story-actions/story-actions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story-actions',
+  imports: [],
+  templateUrl: './story-actions.component.html',
+  styleUrl: './story-actions.component.scss'
+})
+export class StoryActionsComponent {
+
+}
