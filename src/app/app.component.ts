585deb0bf1f267be2f0582ec02753ec8d00e25f7
SnotGit <crocpoms@aol.com>
2025-06-04 16:47:59 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
-import { ConsolePanelComponent } from "./components/navbar/console-panel/console-panel/console-panel.component";
-import { ConsoleTerminalComponent } from "./components/navbar/console-panel/console-terminal/console-terminal.component";
+import { NavbarComponent } from "./core/navbar/navbar/navbar.component";
+import { ConsolePanelComponent } from "./core/navbar/console-panel/console-panel/console-panel.component";
+import { ConsoleTerminalComponent } from "./core/navbar/console-panel/console-terminal/console-terminal.component";
 
 @Component({
   selector: 'app-root',
--- src/app/routes/archives.routes.ts
+++ src/app/routes/archives.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('../components/archives/archives.component').then(m => m.ArchivesComponent)
+    loadComponent: () => import('../features/archives/archives.component').then(m => m.ArchivesComponent)
   }
 ];
