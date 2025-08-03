2a48408be163bc5868e85079a5241941f8f6e89e
SnotGit <crocpoms@aol.com>
2025-08-03 21:01:45 +0200

    update

--- src/features/chroniques/routes/chroniques.routes.ts
+++ src/features/chroniques/routes/chroniques.routes.ts
@@ -14,40 +14,40 @@ export const routes: Routes = [
   
   {
     path: 'mes-histoires/brouillon/edition/nouvelle-histoire',
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/editors/edit-new/edit-new').then(m => m.EditNew),
     resolve: { data: chroniquesResolver },
     canActivate: [authGuard]
   },
   
   {
     path: 'mes-histoires/brouillon/edition/:title',
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/editors/edit-draft/edit-draft').then(m => m.DraftEditor),
     resolve: { data: chroniquesResolver },
     canActivate: [authGuard]
   },
   
   {
     path: 'mes-histoires/publiée/edition/:title',
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/editors/edit-published/edit-published').then(m => m.PublishedEditor),
     resolve: { data: chroniquesResolver },
     canActivate: [authGuard]
   },
 
   {
     path: 'mes-histoires/brouillons', 
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/stories/draft-list/draft-list').then(m => m.DraftList),
     canActivate: [authGuard]
   },
   
   {
     path: 'mes-histoires/publiées',
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/stories/published-list/published-list').then(m => m.PublishedList),
     canActivate: [authGuard]
   },
   
   {
     path: 'mes-histoires',
-    loadComponent: () => import('../components/editor/editor').then(m => m.Editor),
+    loadComponent: () => import('../components/stories/my-stories/my-stories').then(m => m.MyStories),
     canActivate: [authGuard]
   },
 
