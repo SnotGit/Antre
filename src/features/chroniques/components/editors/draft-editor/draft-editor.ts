2bb4465c523b1c4541a263557d830d671f9d49e2
SnotGit <crocpoms@aol.com>
2025-08-03 09:33:27 +0200

    update

---
+++ src/features/chroniques/components/editors/draft-editor/draft-editor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draft-editor',
+  imports: [],
+  templateUrl: './draft-editor.html',
+  styleUrl: './draft-editor.scss'
+})
+export class DraftEditor {
+
+}
---
+++ src/features/chroniques/components/stories/draft-list/draft-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draft-list',
+  imports: [],
+  templateUrl: './draft-list.html',
+  styleUrl: './draft-list.scss'
+})
+export class DraftList {
+
+}
