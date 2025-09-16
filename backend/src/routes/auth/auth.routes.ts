cc51a0943244930966ee6e5a2bc602a1d66aacf0
SnotGit <crocpoms@aol.com>
2025-09-16 23:07:27 +0200

    add something

--- backend/src/routes/auth/auth.routes.ts
+++ backend/src/routes/auth/auth.routes.ts
@@ -15,13 +15,4 @@ router.post('/register', register);
 
 router.get('/validate', authenticateToken, validateToken);
 
-//======= TEST ROUTE =======
-
-router.get('/test', authenticateToken, (req, res) => {
-  res.json({
-    message: 'Route auth protégée accessible !',
-    user: req.user
-  });
-});
-
 export default router;
