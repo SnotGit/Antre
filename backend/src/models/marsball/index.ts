490fd356ecf9d5a3519a4cedeebd9d2d2173ff91
SnotGit <crocpoms@aol.com>
2025-10-13 14:52:51 +0200

    update

---
+++ backend/src/models/marsball/index.ts
@@ -0,0 +1,44 @@
+//======= CATEGORY RESPONSES =======
+
+export interface CategoriesResponse {
+  categories: MarsballCategory[];
+}
+
+export interface CategoryWithChildrenResponse {
+  category: MarsballCategory;
+  children: MarsballCategory[];
+  items: MarsballItem[];
+}
+
+//======= CATEGORY ENTITIES =======
+
+export interface MarsballCategory {
+  id: number;
+  title: string;
+  parentId: number | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+//======= ITEM ENTITIES =======
+
+export interface MarsballItem {
+  id: number;
+  title: string;
+  imageUrl: string;
+  categoryId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+//======= FORM DATA =======
+
+export interface CategoryFormData {
+  title: string;
+  parentId: number | null;
+}
+
+export interface ItemFormData {
+  title: string;
+  categoryId: number;
+}
