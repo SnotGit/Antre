5a7d0737af13b920eb8eefbab40f2ccdde076037
SnotGit <crocpoms@aol.com>
2025-12-08 12:02:18 +0100

    update

--- src/features/marsball/routes/marsball.routes.ts
+++ src/features/marsball/routes/marsball.routes.ts
@@ -7,16 +7,6 @@ export const routes: Routes = [
     loadComponent: () => import('../components/marsball').then(m => m.Marsball)
   },
   {
-    path: 'admin/nouvelle-categorie',
-    loadComponent: () => import('../components/new-category/new-category').then(m => m.NewCategory),
-    canActivate: [authGuard]
-  },
-  {
-    path: 'admin/nouvel-item',
-    loadComponent: () => import('../components/new-item/new-item').then(m => m.NewItem),
-    canActivate: [authGuard]
-  },
-  {
     path: ':categoryId',
     loadComponent: () => import('../components/marsball-category/marsball-category').then(m => m.MarsballCategory)
   }
